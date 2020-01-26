4c408cd7bc1862fd4b2555bd3f33b15a92d17105
rvogel <vogel@hallowelt.biz>
2020-01-26 10:51:09 +0100

    Start Builder

---
+++ src/Builder.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace HalloWelt\MediaWiki\Lib\MediaWikiXML;
+
+use DOMDocument;
+
+class Builder {
+
+	/**
+	 *
+	 * @var DOMDocument
+	 */
+	private $dom = null;
+
+	public function __construct() {
+	}
+
+	/**
+	 *
+	 * @param string $destFilepath
+	 * @return boolean
+	 */
+	public function buildAndSave( $destFilepath ) {
+
+	}
+
+	/**
+	 * @return DOMDocument
+	 */
+	public function build() {
+		
+	}
+}
