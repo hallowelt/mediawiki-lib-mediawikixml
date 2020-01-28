edb03938123ca4f0c35d47e8dd7c38afdecb4b57
rvogel <vogel@hallowelt.biz>
2020-01-28 16:40:34 +0100

    Start implementation of Builder

--- src/Builder.php
+++ src/Builder.php
@@ -3,6 +3,7 @@
 namespace HalloWelt\MediaWiki\Lib\MediaWikiXML;
 
 use DOMDocument;
+use DOMElement;
 
 class Builder {
 
@@ -12,8 +13,11 @@ class Builder {
 	 */
 	private $dom = null;
 
-	public function __construct() {
-	}
+	/**
+	 *
+	 * @var array
+	 */
+	private $pages = [];
 
 	/**
 	 *
@@ -21,13 +25,60 @@ class Builder {
 	 * @return boolean
 	 */
 	public function buildAndSave( $destFilepath ) {
-
+		$this->build();
+		$writtenBytes = $this->dom->save( $destFilepath );
+		return $writtenBytes !== false;
 	}
 
 	/**
 	 * @return DOMDocument
 	 */
 	public function build() {
-		
+		$this->initDOM();
 	}
+
+	private $mediaWikiXMLStub = <<<HERE
+<mediawiki xmlns="http://www.mediawiki.org/xml/export-0.10/" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="http://www.mediawiki.org/xml/export-0.10/ http://www.mediawiki.org/xml/export-0.10.xsd" version="0.10" xml:lang="en">
+</mediawiki>
+HERE;
+
+	private function initDOM() {
+		$this->dom = new DOMDocument();
+		$this->dom->loadXML( $this->mediaWikiXMLStub );
+	}
+
+	/**
+	 *
+	 * @param string $pagetitle
+	 * @param string $wikitext
+	 * @param string $timestamp
+	 * @param string $username
+	 * @param string $model
+	 * @param strgin $format
+	 * @return Builder
+	 */
+	public function addRevision( $pagetitle, $wikitext, $timestamp = '', $username = '', $model = 'wikitext' , $format = 'text/x-wiki' ) {
+		$this->pages[$pagetitle];
+		return $this;
+	}
+
+	/**
+	 *
+	 * @param string $pagetitle
+	 * @param DOMElement $customEl
+	 * @return Builder
+	 */
+	public function addCustomPageElement( $pagetitle, DOMElement $customEl ) {
+		return $this;
+	}
+
+	/**
+	 *
+	 * @param string $elementName
+	 * @return DOMElement
+	 */
+	public function createCustomPageElement( $elementName ) {
+		return $this->dom->createElement( $elementName );
+	}
+
 }
