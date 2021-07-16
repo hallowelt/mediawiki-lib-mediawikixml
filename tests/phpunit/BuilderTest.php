567f23c858e804038b58470b134b9bdf94e9e60b
rvogel <vogel@hallowelt.biz>
2021-07-16 15:04:15 +0200

    Avoid issues with special chars in page titles
    ERM24997

---
+++ tests/phpunit/BuilderTest.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace HalloWelt\MediaWiki\Lib\MediaWikiXML\Tests;
+
+use HalloWelt\MediaWiki\Lib\MediaWikiXML\Builder;
+use PHPUnit\Framework\TestCase;
+
+class BuilderTest extends TestCase {
+
+	public function testBuild() {
+		$actualFile = sys_get_temp_dir() . '/output.xml';
+		$exprectedFile = __DIR__ . '/data/expected1.xml';
+
+		$builder = new Builder();
+		$builder->addRevision( "Coffee & Tea", "raffiné" );
+		$builder->buildAndSave( $actualFile );
+
+		$this->assertXmlFileEqualsXmlFile( $exprectedFile, $actualFile );
+	}
+}
